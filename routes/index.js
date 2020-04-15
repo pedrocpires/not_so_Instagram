b3a0afbe2556b24c2da56497204b28a5a18947eb
Pedro Pires <pedro.cpires@hotmail.com>
2020-04-15 14:03:02 -0300

    Create project structure with express and ejs

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'not_so_Instagram' });
+});
+
+module.exports = router;
